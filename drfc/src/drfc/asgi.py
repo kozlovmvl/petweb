36b62b3fa54708d2d2ab263b102e26b47ca7655d
kozlovmv <mikhail.kozlov@eventplatform.ru>
2025-09-27 14:03:37 +0300

    ruff format

--- drfc/src/drfc/asgi.py
+++ drfc/src/drfc/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drfc.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "drfc.settings")
 
 application = get_asgi_application()
