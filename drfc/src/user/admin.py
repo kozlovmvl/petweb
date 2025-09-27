dfdf06b8aa92852868fb633932265532625a9847
kozlovmv <mikhail.kozlov@eventplatform.ru>
2025-09-27 15:50:32 +0300

    User models:
    - user
    - city
    - migrations
    - admin

--- drfc/src/user/admin.py
+++ drfc/src/user/admin.py
@@ -1,1 +1,17 @@
-# Register your models here.
+from django.contrib import admin
+
+from user.models import City, User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("id", "username")
+    search = ("username", "email", "last_name", "first_name")
+    filter_horizontal = ("cities",)
+    readonly_fields = ("id",)
+
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
--- drfc/src/user/models.py
+++ drfc/src/user/models.py
@@ -1,1 +1,32 @@
-# Create your models here.
+import uuid
+
+from django.db import models
+
+
+class User(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True)
+    username = models.CharField(max_length=20, unique=True)
+    email = models.EmailField(unique=True)
+    last_name = models.CharField(max_length=20, blank=True)
+    first_name = models.CharField(max_length=20, blank=True)
+    cities = models.ManyToManyField("City", blank=True, related_name="users")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+    def __str__(self) -> str:
+        return str(self.username)
+
+
+class City(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+
+    class Meta:
+        verbose_name = "Город"
+        verbose_name_plural = "Города"
+
+    def __str__(self) -> str:
+        return str(self.name)
