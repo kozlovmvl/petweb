4f86c1909eb52d355047f088b0eea94e60ac5876
kozlovmv <mikhail.kozlov@eventplatform.ru>
2025-10-05 10:16:17 +0300

    Squashed commit of the following:
    commit f5b6f03fc0881e07127524fa97a54c115e6e5afb
    Author: kozlovmv <mikhail.kozlov@eventplatform.ru>
    Date:   Sun Oct 5 10:13:45 2025 +0300
    
        User crud api
    
        - create
        - partial update

--- drfc/src/user/serializers.py
+++ drfc/src/user/serializers.py
@@ -1,9 +1,33 @@
-from rest_framework import serializers
+from rest_framework import serializers, validators
 
-from user.models import User
+from user.models import City, User
 
 
-class UserListSerializer(serializers.ModelSerializer[User]):
+class CityReadSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = City
+        fields = ("id", "name")
+
+
+class UserReadSerializer(serializers.ModelSerializer[User]):
     class Meta:
         model = User
-        fields = ("id", "username", "last_name", "first_name")
+        fields = ("id", "username", "last_name", "first_name", "cities")
+
+    cities = CityReadSerializer(many=True)
+
+
+class UserCreateSerializer(serializers.ModelSerializer[User]):
+    class Meta:
+        model = User
+        fields = ("username", "last_name", "first_name", "cities")
+
+    username = serializers.CharField(
+        validators=[validators.UniqueValidator(queryset=User.objects.all())]
+    )
+
+
+class UserUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("last_name", "first_name", "cities")
--- drfc/src/user/views.py
+++ drfc/src/user/views.py
@@ -1,13 +1,34 @@
-from rest_framework import filters
+from django.db.models import Prefetch
+from rest_framework import filters, status
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
-from user.models import User
-from user.serializers import UserListSerializer
+from user.models import City, User
+from user.serializers import (
+    UserCreateSerializer,
+    UserReadSerializer,
+    UserUpdateSerializer,
+)
 
 
 class UserView(ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserListSerializer
+    queryset = User.objects.all().prefetch_related(
+        Prefetch("cities", queryset=City.objects.all().order_by("name"))
+    )
+    serializer_class = UserReadSerializer
     filter_backends = [filters.SearchFilter, filters.OrderingFilter]
     search_fields = ["username", "last_name", "first_name"]
     ordering_fields = ["username", "last_name", "first_name"]
+
+    def create(self, request):
+        serializer = UserCreateSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        created_user = serializer.save()
+        return Response(created_user.id, status=status.HTTP_201_CREATED)
+
+    def partial_update(self, request, pk=None):
+        user = self.get_object()
+        serializer = UserUpdateSerializer(user, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(status=status.HTTP_200_OK)
